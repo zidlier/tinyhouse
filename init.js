9e34ac102a2094379a4ccda0614cd00787a44107
Patrick Garcia <patrick@skyciv.com>
2021-10-06 22:12:24 +0800

    Added Update renderer function

--- init.js
+++ init.js
@@ -13,12 +13,9 @@ var TINY_HOUSE = (function () {
 		//TODO for the dynamics from the inputs
 		var data = INDEX.getData()
 
-
-
 		// (buildingLength, buildingWidth, eaveHeight, roofApex, roofOverhang)
-		let s3d_model = TINY_HOUSE.framing.generateWallFramingS3DModel(6,10,3,5,0.8)
+		s3d_model = TINY_HOUSE.framing.generateWallFramingS3DModel(6,10,3,5,0.8)
 
-		
 		viewer.model.set(s3d_model);
 		viewer.model.buildStructure();
 		viewer.render();
@@ -30,7 +27,17 @@ var TINY_HOUSE = (function () {
 	}
 	
 
-	functions.updateRenderer
+	functions.getS3DModel = function () {
+		return s3d_model
+	}
+
+	functions.updateRenderer = function (s3dModel) {
+		s3d_model = s3dModel
+		functions.getViewer().model.set(s3d_model);
+		functions.getViewer().model.buildStructure();
+		functions.getViewer().render();
+
+	}
 
 
 	return functions;

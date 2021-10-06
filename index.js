b69a5447f2fac6756e0946074caae1858dd71f10
Albert <albert@skyciv.com>
2021-10-06 18:46:21 +0800

    revised logo

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
     TINY_HOUSE.init();
+
+    jQuery('#main-tab .item').tab();
+
+
+jQuery('#results_button').click(function () {
+
+    jQuery('#results').modal('show');
+
+});
+
+
+
 });
